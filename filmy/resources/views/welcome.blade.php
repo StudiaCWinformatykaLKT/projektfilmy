c802ccd614f0fd2ba40c2be327a37e7f30ce9137
LidiaDzierla <dzierlatka04@gmail.com>
2025-03-16 12:56:19 +0100

    poprawia błędów

--- filmy/resources/views/welcome.blade.php
+++ filmy/resources/views/welcome.blade.php
@@ -1,26 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ $movieData['title'] }}</title>
+    <title>Welcome</title>
 </head>
 <body>
-<h2>{{ $movieData['title'] }}</h2>
-<p><strong>Overview:</strong> {{ $movieData['overview'] }}</p>
-<p><strong>Release Date:</strong> {{ $movieData['release_date'] }}</p>
-<p><strong>Runtime:</strong> {{ $movieData['runtime'] }} minutes</p>
-<p><strong>Genres:</strong>
-    @if(isset($movieData['genres']) && is_array($movieData['genres']))
-        {{ implode(', ', array_column($movieData['genres'], 'name')) }}
-    @endif
-</p>
-<p><strong>Rating:</strong> {{ $movieData['vote_average'] }}</p>
-<p><strong>Production Companies:</strong>
-    @if(isset($movieData['production_companies']) && is_array($movieData['production_companies']))
-        {{ implode(', ', array_column($movieData['production_companies'], 'name')) }}
-    @endif
-</p>
-@if(isset($movieData['poster_path']))
-    <img src="https://image.tmdb.org/t/p/w500{{ $movieData['poster_path'] }}" alt="Poster">
-@endif
+<h2>Welcome to the Movie App</h2>
 </body>
 </html>
+
